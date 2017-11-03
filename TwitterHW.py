32d979dbceee3000f013deacbd7b59a07523c8be
Lilian Sheu <lilians@umich.edu>
2017-11-03 01:45:39 -0400

    basically done

--- TwitterHW.py
+++ TwitterHW.py
@@ -10,6 +10,15 @@ import re
 import tweepy
 import twitter_info # still need this in the same directory, filled out
 
+import sys
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
+
 consumer_key = twitter_info.consumer_key
 consumer_secret = twitter_info.consumer_secret
 access_token = twitter_info.access_token
@@ -24,9 +33,9 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 CACHE_FNAME = "twitter_cache.json"
 try:
     cache_file = open(CACHE_FNAME,'r')
-        cache_contents = cache_file.read()
-        cache_file.close()
-        CACHE_DICTION = json.loads(cache_contents)
+    cache_contents = cache_file.read()
+    cache_file.close()
+    CACHE_DICTION = json.loads(cache_contents)
 except:
     CACHE_DICTION = {}
 
@@ -35,10 +44,18 @@ except:
 # Here, define a function called get_tweets that searches for all tweets referring to or by "umsi"
 # Your function must cache data it retrieves and rely on a cache file!
 
-
 def get_tweets():
-##YOUR CODE HERE
-
+    if 'umsi' in CACHE_DICTION:
+        print('using cached data')
+        twitter_results = CACHE_DICTION['umsi']
+    else:
+        print('getting data from internet')
+        twitter_results = api.user_timeline('umsi')
+        # writing the new file
+        f = open(CACHE_FNAME, 'w')
+        f.write(json.dumps(CACHE_DICTION))
+        f.close()
+    return twitter_results
 
 
 ## [PART 2]
@@ -53,19 +70,25 @@ def get_tweets():
 # Below we have provided interim outline suggestions for what to do, sequentially, in comments.
 
 # 1 - Make a connection to a new database tweets.sqlite, and create a variable to hold the database cursor.
-
+conn = sqlite3.connect('tweets.sqlite')
+cur = conn.cursor()
 
 # 2 - Write code to drop the Tweets table if it exists, and create the table (so you can run the program over and over), with the correct (4) column names and appropriate types for each.
 # HINT: Remember that the time_posted column should be the TIMESTAMP data type!
-
+cur.execute('DROP TABLE IF EXISTS Tweets')
+cur.execute('CREATE TABLE Tweets (tweet_id TEXT, author TEXT, time_posted TEXT, tweet_text TEXT, retweets TEXT)')
 # 3 - Invoke the function you defined above to get a list that represents a bunch of tweets from the UMSI timeline. Save those tweets in a variable called umsi_tweets.
+umsi_tweets = get_tweets()
 
-
-# 4 - Use a for loop, the cursor you defined above to execute INSERT statements, that insert the data from each of the tweets in umsi_tweets into the correct columns in each row of the Tweets database table.
-
+# 4 - Use a for loop, the cursor you defined above to execute INSERT statements, 
+# that insert the data from each of the tweets in umsi_tweets into the correct columns 
+# in each row of the Tweets database table.
+for twt in umsi_tweets:
+    tup = twt['id'], twt['user']['screen_name'], twt['created_at'], twt['text'], twt['retweet_count']
+    cur.execute('INSERT INTO Tweets (tweet_id, author, time_posted, tweet_text, retweets) VALUES(?, ?, ?, ?, ?)',tup)
 
 #  5- Use the database connection to commit the changes to the database
-
+conn.commit()
 # You can check out whether it worked in the SQLite browser! (And with the tests.)
 
 ## [PART 3] - SQL statements
@@ -75,12 +98,18 @@ def get_tweets():
     # Mon Oct 09 15:45:45 +0000 2017 - RT @MikeRothCom: Beautiful morning at @UMich - It’s easy to forget to
     # take in the view while running from place to place @umichDLHS  @umich…
 # Include the blank line between each tweet.
-
-
+cur.execute('SELECT time_posted, tweet_text FROM Tweets')
+all_res = cur.fetchall()
+for t in all_res:
+    uprint(t[0] + ' - ' + t[1] +'\n')
 # Select the author of all of the tweets (the full rows/tuples of information) that have been retweeted MORE
 # than 2 times, and fetch them into the variable more_than_2_rts.
 # Print the results
+cur.execute('SELECT author FROM Tweets WHERE retweets > 2')
+more_than_2_rts = cur.fetchall()
+print('more_than_2_rts - %s ' % set(more_than_2_rts))
 
+cur.close()
 
 
 if __name__ == "__main__":
